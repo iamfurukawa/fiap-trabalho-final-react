a8f23267dd5166b6e4e47ddb4cdd66a2b3fec7ec
Vinicius Furukawa Carvalho <vinicius.furukawa.carvalho@gmail.com>
2023-07-03 00:18:33 -0300

    fixing bug

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 import { Person as PersonParam } from "../../shared/person";
 
-const FreelaDetalhesPage = (param: PersonParam) => {
-  const { foto, nome, tecnologias, ultimosTrabalhos } = param;
+const FreelaDetalhesPage = () => {
+  //const { foto, nome, tecnologias, ultimosTrabalhos } = param;
   const { freelaId } = useParams();
   return <>detalhes de cada freela: {freelaId}</>;
 };
